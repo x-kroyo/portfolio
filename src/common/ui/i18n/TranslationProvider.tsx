526690d96a05ff56a5d10074854cf611fff72cd4
x-kroyo <mr.ayoubmarghad@gmail.com>
2025-09-21 18:24:49 +0100

    chore: enhance TranslationProvider to manage loading state during initialization

--- src/common/ui/i18n/TranslationProvider.tsx
+++ src/common/ui/i18n/TranslationProvider.tsx
@@ -1,11 +1,16 @@
 import { I18nextProvider } from "react-i18next";
 import { i18nInitializer } from ".";
 import i18next from "i18next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const TranslationProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [initialized, setInitialized] = useState(false);
   useEffect(() => {
-    i18nInitializer();
+    i18nInitializer().then(() => {
+      setInitialized(true);
+    });
   }, []);
-  return <I18nextProvider i18n={i18next}>{children}</I18nextProvider>;
+  return (
+    initialized ? <I18nextProvider i18n={i18next}>{children}</I18nextProvider> : "Loading..."
+  );
 };
