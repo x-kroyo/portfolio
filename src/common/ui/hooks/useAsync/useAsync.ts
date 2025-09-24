9e1cbd257fe83121d5d9f0a424a35bce0d948886
x-kroyo <mr.ayoubmarghad@gmail.com>
2025-09-24 19:00:45 +0100

    feat: add useAsync and useAsyncFn hooks async operation (copied from react-use library)

---
+++ src/common/ui/hooks/useAsync/useAsync.ts
@@ -0,0 +1,18 @@
+import { DependencyList, useEffect } from 'react';
+import { FunctionReturningPromise } from './types';
+import useAsyncFn from './useAsyncFn';
+
+export default function useAsync<T extends FunctionReturningPromise>(
+  fn: T,
+  deps: DependencyList = []
+) {
+  const [state, callback] = useAsyncFn(fn, deps, {
+    loading: true,
+  });
+
+  useEffect(() => {
+    callback();
+  }, [callback]);
+
+  return state;
+}
