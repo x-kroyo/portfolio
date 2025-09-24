a9503a8188c95b94f3e29691d99300006ba893bc
x-kroyo <mr.ayoubmarghad@gmail.com>
2025-09-24 17:30:25 +0100

    feat: retrieve data from users API service

--- src/features/users/infra/adapters/UserRepository.ts
+++ src/features/users/infra/adapters/UserRepository.ts
@@ -1,11 +1,15 @@
 import { User } from "../../domain/entities/User";
 import { UserRepositoryPort } from "../../domain/ports/UserRepositoryPort";
 import { injectable } from "inversify";
+import axiosApiClient from "@/src/common/infra/api/axiosApiClient";
+import queryClient from "@/src/common/infra/api/queryClient";
 
 @injectable()
 export class UserRepository implements UserRepositoryPort {
   async findUserById(userId: string): Promise<User | null> {
-    // TODO: Replace with actual data retrieval logic (e.g., database query)
-    return { id: userId, name: "John Doe" };
+    return queryClient.fetchQuery({
+      queryKey: ['user', userId],
+      queryFn: () => axiosApiClient.get<User>(`users/${userId}`)
+    });
   }
 }
