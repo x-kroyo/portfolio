dbd32d13c3169e0d3a5ae469091026c7c260fb01
x-kroyo <mr.ayoubmarghad@gmail.com>
2025-09-25 01:21:38 +0100

    refactor: simplify constructors in value objects by directly initializing private properties

--- src/features/users/domain/valueobjects/PlainPassword.ts
+++ src/features/users/domain/valueobjects/PlainPassword.ts
@@ -1,11 +1,6 @@
 export class PlainPassword {
-  private readonly _value: string;
   
-  constructor(value: string) {
-    if (value.length < 8) {
-      throw new Error("Password must be at least 8 characters long");
-    }
-    this._value = value;
+  constructor(private readonly _value: string) {
   }
 
   value() {
