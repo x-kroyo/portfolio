731a3b2c4f444947cd8cde39ae0b9dfbec9abcb0
x-kroyo <mr.ayoubmarghad@gmail.com>
2025-12-28 20:22:46 +0100

    fix: correct import path of generated client

--- src/features/users/infra/api/http/client.config.ts
+++ src/features/users/infra/api/http/client.config.ts
@@ -1,5 +1,5 @@
 import { publicApiAxiosInstance } from "@/src/common/infra/http/publicApiAxiosInstance";
-import { client, CreateClientConfig } from "@/src/features/users/infra/http/generated/client.gen";
+import { CreateClientConfig } from "@/src/features/users/infra/api/http/generated/client";
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
